ed0d60bd7055c44857a7a74b4fca8b8d0625ae3f
firaschkoundali <firas.chkoundali@esprit.tn>
2020-04-10 14:48:06 -0700

    pour le moment elle contient gestion de vie et de score

--- utilitaire.c
+++ utilitaire.c
@@ -5,4 +5,23 @@
 #include "utilitaire.h"
 #include "preso.h"
 
+void initialiservie(texte *textevie ,int *vie)
+{
+(*textevie).couleur= {255,255,255};
+(*textevie).police = TTF_OpenFont("arial.ttf", 30);
+(*textevie).texte = TTF_RenderText_Blended(police,"vie", couleurBlanche);
+(*textevie).postexte.x=300;
+(*textevie).postexte.y=100;
+(*vie)=3;
+}
+
+void initialiserscore(texte *textescore,int *score)
+{
+(*textescore).couleur= {255,255,255};
+(*textescore).police = TTF_OpenFont("arial.ttf", 30);
+(*textescore).texte = TTF_RenderText_Blended(police,"score", couleurBlanche);
+(*textescore).postexte.x=20;
+(*textescore).postexte.y=100;
+(*score)=0000;
+}
 
