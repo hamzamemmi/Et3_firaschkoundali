f2a589e87549b0a1fa46c6d6c62802bc998e84ed
firaschkoundali <firas.chkoundali@esprit.tn>
2020-04-14 15:44:46 -0700

    gestion vie

--- utilitaire.c
+++ utilitaire.c
@@ -2,25 +2,54 @@
 #include <stdlib.h>
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_ttf.h>
+#include "utilitaire.h"
 #include "preso.h"
+void initText(Text* T)
+{    
+    T->couleurTxt.r = 0; 
+    T->couleurTxt.g = 0; 
+    T->couleurTxt.b = 0;
 
-void initialiservie(texte *textevie ,int *vie)
-{
-(*textevie).couleur= {255,255,255};
-(*textevie).police = TTF_OpenFont("arial.ttf", 30);
-(*textevie).texte = TTF_RenderText_Blended(police,"vie", couleurBlanche);
-(*textevie).postexte.x=300;
-(*textevie).postexte.y=100;
-(*vie)=3;
+    strcpy(T->txt, "");
+    T->positionText.x = 200;
+    T->positionText.y = 50;    
 }
 
-void initialiserscore(texte *textescore,int *score)
+int loadFont(Text* T, char* path)
 {
-(*textescore).couleur= {255,255,255};
-(*textescore).police = TTF_OpenFont("arial.ttf", 30);
-(*textescore).texte = TTF_RenderText_Blended(police,"score", couleurBlanche);
-(*textescore).postexte.x=20;
-(*textescore).postexte.y=100;
-(*score)=0000;
+    TTF_Font* police = NULL;
+
+    if(TTF_Init() == -1) {
+        printf("Erreur d'initialisation de TTF_Init : %s\n", TTF_GetError());
+        return -1;
+    }
+    police = TTF_OpenFont(path,20);
+    if (police == NULL) {
+        printf("Unable to load Font: %s\n", SDL_GetError());
+        return (-1);
+    }
+    
+    T->police = police;
+    return (0);
+}
+
+
+void update_txt(Text* T, int vies)
+{
+    sprintf(T->txt,"*** VIES = %d ***",vies);
+}
+
+void displayText(Text T,SDL_Surface *screen)
+{   SDL_Surface* textSurface = NULL;    
+    
+     textSurface=TTF_RenderText_Solid(T.police,T.txt,T.couleurTxt);
+     SDL_BlitSurface(textSurface,NULL,screen,&(T.positionText));
+
+}
+
+void freeText(Text* T)
+{
+    TTF_CloseFont(T->police); 
+    TTF_Quit();    
 }
 
