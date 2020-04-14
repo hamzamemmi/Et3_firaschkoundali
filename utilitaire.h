aae80e717682f44de75e6bc40b2e2d94338cdc96
firaschkoundali <firas.chkoundali@esprit.tn>
2020-04-14 15:42:08 -0700

    ...

--- utilitaire.h
+++ utilitaire.h
@@ -1,12 +1,27 @@
 #ifndef UTILITAIRE_H_INCLUDED
 #define UTILITAIRE_H_INCLUDED
 #include "perso.h"
-typedef struct
-{	SDL_Color couleur;
-	SDL_Surface *texte;
-	TTF_Font *police=NULL;
-	SDL_Rect postexte;
-}texte;
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+#include <string.h>
+struct text {
+    SDL_Rect positionText;
+    char txt[20];
+    SDL_Color couleurTxt;
+    TTF_Font* police;
+};
+typedef struct text Text;
+
+void initText(Text* T);
+void update_txt(Text* T, int vies);
+void displayText(Text T,SDL_Surface *screen);
+void freeText(Text* T);
+int loadFont(Text* T, char* path);
+void initialiserscore(texte *textescore,int score);
+void miseajourcore(texte *textescore,int *score);
+void miseajourtemps(texte *temps);
+void afficherscore(texte textescore,int score);
+void affichertemps(texte temps);
 
 
 
